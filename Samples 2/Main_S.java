7610967d5eb5f19ddbaff9ba9f86d90723c5d056
24DPESvan <A240393ES@rvt.lv>
2026-04-04 12:13:45 +0300

    2 Uzd

---
+++ Samples 2/Main_S.java
@@ -0,0 +1,33 @@
+public class Main_S {
+    public static void main(String[] args) {
+
+        Account account1 = new Account("Eric", 1L, 34, 2000);
+        Account account2 = new Account("Star", 2L, 34, 500);
+        Account account3 = new Account("George", 3L, 34, 4000);
+
+        Bank bank = new Bank();
+
+        bank.addAccount(account1);
+        bank.addAccount(account2);
+        bank.addAccount(account3);
+
+        bank.addBalance(2L, 1000);
+        bank.removeAccount(1L);
+
+        System.out.println("Accounts in bank: " + bank.getAccountCount());
+        System.out.println("Current capacity: " + bank.getCapacity());
+        System.out.println("Total bank balance: " + bank.getTotalBalance());
+
+        Account found = bank.findAccount(3L);
+
+        if (found != null) {
+            System.out.println("\nFound account:");
+            System.out.println("ID: " + found.getId());
+            System.out.println("Name: " + found.getName());
+            System.out.println("Age: " + found.getAge());
+            System.out.println("Balance: " + found.getBalance());
+        } else {
+            System.out.println("\nUser was not found.");
+        }
+    }
+}
